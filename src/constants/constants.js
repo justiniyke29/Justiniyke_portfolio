f81266b521ea0a6254408c72716f9687510a4bc1
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2023-01-04 01:04:52 +0100

    added new project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,44 +1,60 @@
 export const projects = [
   {
-    title: 'E-Commerce',
-     description:"A modern E-Commerce website with Stripe payment options, ability to add and remove items from the sites, edit prices and many other features. ",
-    image: '/images/ecommerce.png',
-    tags: ['React', 'Next','Sanity', 'JavaScript'],
-    source: 'https://github.com/justiniyke29/shopping-mall',
-    visit: 'https://justiniyke-shoppingmall.vercel.app',
+    title: "E-Commerce",
+    description:
+      "A modern E-Commerce website with Stripe payment options, ability to add and remove items from the sites, edit prices and many other features. ",
+    image: "/images/ecommerce.png",
+    tags: ["React", "Next", "Sanity", "JavaScript"],
+    source: "https://github.com/justiniyke29/shopping-mall",
+    visit: "https://justiniyke-shoppingmall.vercel.app",
     id: 0,
   },
   {
-    title: 'Youtube_Clone',
-    description: "Justinmedia is a youtube clone created with Reactjs, Rapidapi and Material UI 5. " ,
-      image: '/images/youtube-clone.jpg',
-      tags: ['Material UI 5', 'RapidApi', 'React'],
-    source: 'https://github.com/justiniyke29/youtube_clone',
-    visit: 'https://justinmedia.netlify.app',
+    title: "Chicken City",
+    description:
+      " Chicken City is a food delivery E-commerce web application built with React.js , Tailwind CSS ,Redux , flutterwave SDK as payment gateway ,framer-motion and firebase as its backend.",
+    image: "/images/chickencity.png",
+    tags: ["React", "Flutterwave sdk", "Firebase", "JavaScript"],
+    source: "https://github.com/justiniyke29/Full_Stack_Foody",
+    visit: "https://full-stack-foody.vercel.app/",
+    id: 0,
+  },
+  {
+    title: "Youtube_Clone",
+    description:
+      "Justinmedia is a youtube clone created with Reactjs, Rapidapi and Material UI 5. ",
+    image: "/images/youtube-clone.jpg",
+    tags: ["Material UI 5", "RapidApi", "React"],
+    source: "https://github.com/justiniyke29/youtube_clone",
+    visit: "https://justinmedia.netlify.app",
     id: 1,
   },
   {
-    title: 'Justin-Fries',
-    description: "A modern E-Commerce website with responsive UI and amazing features",
-      image: '/images/justin-fries.jpg',
-      tags: ['Firebase', 'Material UI', 'React'],
-    source: 'https://github.com/justiniyke29/justin-fries',
-    visit: 'https://justin-fries.netlify.app',
+    title: "Justin-Fries",
+    description:
+      "A modern E-Commerce website with responsive UI and amazing features",
+    image: "/images/justin-fries.jpg",
+    tags: ["Firebase", "Material UI", "React"],
+    source: "https://github.com/justiniyke29/justin-fries",
+    visit: "https://justin-fries.netlify.app",
     id: 2,
   },
   {
-    title: 'Portfolio',
+    title: "Portfolio",
     description: "A portfolio containing some of my achievements and works.",
-      image: '/images/port.png',
-      tags: ['Next', 'Material UI5'],
-    source: 'https://github.com/justiniyke29/justiniyke_portfolio',
-    visit: 'https://justinikechukwu.netlify.app',
+    image: "/images/port.png",
+    tags: ["Next", "Material UI5"],
+    source: "https://github.com/justiniyke29/justiniyke_portfolio",
+    visit: "https://justinikechukwu.netlify.app",
     id: 3,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2019, text: 'Started my journey', },
-  { year: 2020, text: 'Did internship', },
-  { year: 2021, text: 'Worked with different Startups', }
+  { year: 2020, text: "Started my journey" },
+  {
+    year: 2021,
+    text: "Did internship and Worked with different Startups and enterprise",
+  },
+  { year: 2022, text: "Started an Online Community for developers" },
 ];

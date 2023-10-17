1de435cd5d8676584f83b9d74095bd54837afe32
Justin Ikechukwu Anyanwu <justiniyke1995@gmail.com>
2023-10-17 17:51:42 +0100

    removed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,28 +9,34 @@ export const projects = [
     source: "",
     visit: "https://myeventpod.com",
     id: 0,
-  }, 
-   {
+  },
+  {
     title: "Xcursions",
-    description: "At Xcursions, we are removing barriers to having a premium travel experience. Our travel processes are timely, and are handled by experts, to give you the experience you truly deserve. Travelling just got easier!",
+    description:
+      "At Xcursions, we are removing barriers to having a premium travel experience. Our travel processes are timely, and are handled by experts, to give you the experience you truly deserve. Travelling just got easier!",
     image: "/images/xcursions.png",
-    tags: ["Nextjs", "Typescript","RTK Query", "Google OAuth", "Facebook Auth"],
+    tags: [
+      "Nextjs",
+      "Typescript",
+      "RTK Query",
+      "Google OAuth",
+      "Facebook Auth",
+    ],
     showSource: false,
     visit: "https://staging.xcursions.ng",
     id: 2,
-
   },
-  {
-    title: "E-Commerce",
-    description:
-      "A modern E-Commerce website with Stripe payment options, ability to add and remove items from the sites, edit prices and many other features. ",
-    image: "/images/ecommerce.png",
-    tags: ["Next", "Sanity", "Stripe", "JavaScript"],
-    showSource: true,
-    source: "https://github.com/justiniyke29/shopping-mall",
-    visit: "https://justiniyke-shoppingmall.vercel.app",
-    id: 1,
-  }
+  // {
+  //   title: "E-Commerce",
+  //   description:
+  //     "A modern E-Commerce website with Stripe payment options, ability to add and remove items from the sites, edit prices and many other features. ",
+  //   image: "/images/ecommerce.png",
+  //   tags: ["Next", "Sanity", "Stripe", "JavaScript"],
+  //   showSource: true,
+  //   source: "https://github.com/justiniyke29/shopping-mall",
+  //   visit: "https://justiniyke-shoppingmall.vercel.app",
+  //   id: 1,
+  // }
 
   // {
   //   title: "Chicken City",

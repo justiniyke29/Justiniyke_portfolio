55fbc59aff72bfbd7baaf76d7fab9751060ae5dd
Justin Ikechukwu Anyanwu <justiniyke1995@gmail.com>
2023-10-18 12:48:56 +0100

    fix technologies

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,50 +1,70 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
- <Section id='tech'>
-  <SectionDivider />
-  <SectionTitle>Technologies</SectionTitle>
-  <SectionText>
-    I've worked with a range of Technologies in the development world.
-    From Front-end to Back-end and Design.
-  </SectionText>
-  <List>
-    <ListItem>
-      <DiReact size="3rem"/>
-      <ListContainer>
-        <ListTitle>Front-End</ListTitle>
-        <ListParagraph>
-          Experience with <br />
-          React.js, Next.js, Javascript <br />
-          Tailwind, Bootstrap, Css
-        </ListParagraph>
-      </ListContainer>
-    </ListItem>
-    <ListItem>
-      <DiFirebase size="3rem"/>
-      <ListContainer>
-        <ListTitle>Back-End</ListTitle>
-        <ListParagraph>
-          Experience with <br />
-          Node, Express and MongoDB
-        </ListParagraph>
-      </ListContainer>
-    </ListItem>
-    <ListItem>
-      <DiZend size="3rem"/>
-      <ListContainer>
-        <ListTitle>UI/UX</ListTitle>
-        <ListParagraph>
-          Experience with <br />
-          tools like Figma, Material UI, Styled Components
-        </ListParagraph>
-      </ListContainer>
-    </ListItem>
-  </List>
- </Section>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of Technologies in the development world. From
+      Front-end to Back-end and Design.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js, Next.js, Javascript <br />
+            Typescript, Sass, Less <br />
+            GraphQL, Chakra, Material Ui <br />
+            Tailwind, Bootstrap, Css
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node, Express and MongoDB
+          </ListParagraph>
+        </ListContainer>
+        <ListContainer>
+          <ListTitle>Mobile</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React Native
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with tools like <br />
+            Figma, Adobe, Sketch
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
